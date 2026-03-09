22e8546f4e8f17ac2f89cbc83bfd2f061a4abc5b
shoriful-dev <cmshohelrana50@gmail.com>
2026-03-09 12:53:12 +0600

    add vercel config

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
